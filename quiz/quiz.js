cc2e054e26a79dd3974dd4cae3d3eadbf9e38245
Lisa Dunlap <lisabdunlap@berkeley.edu>
2019-01-06 17:21:26 +0800

    probably broke the quiz

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -216,6 +216,23 @@
    var selections = []; //Array containing user choices
    var quiz = $('#quiz'); //Quiz div object
 
+   //shuffle questions
+   Array.prototype.shuffle = function() {
+    var input = this;
+
+    for (var i = input.length-1; i >=0; i--) {
+
+        var randomIndex = Math.floor(Math.random()*(i+1));
+        var itemAtIndex = input[randomIndex];
+
+        input[randomIndex] = input[i];
+        input[i] = itemAtIndex;
+    }
+    return input;
+  }
+
+  questions.shuffle();
+
    // Display initial question
    displayNext();
 
